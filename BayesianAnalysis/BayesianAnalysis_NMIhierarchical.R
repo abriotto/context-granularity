e39db8568e1d329dee9e0986068784ecbeff4731
Kristina Kobrock <49274795+kristinakobrock@users.noreply.github.com>
2026-05-27 10:33:07 +0200

    added complete Bayesian analysis

--- BayesianAnalysis/BayesianAnalysis_NMIhierarchical.R
+++ BayesianAnalysis/BayesianAnalysis_NMIhierarchical.R
@@ -1,5 +1,10 @@
+## Title: Analysis of entropy scores per context granularity
+## Description: Analyses reported in section 3.2 of the paper
+## Author: Kristina Kobrock
+## Contact: kristina.kobrock@uni-osnabrueck.de
+## Last Edit: 2026-05-27
+
 setwd(normalizePath(dirname(rstudioapi::getActiveDocumentContext()$path)))
-df <- read.csv('data_for_R_NMI_hierarchical.csv')
 
 # BEST package is no longer active, but can still be downloaded from CRAN archive
 # Download package tarball from CRAN archive
@@ -15,100 +20,278 @@ library(tidyverse)
 
 # NMI----------------------------
 
-# df_NMI_fine <- df %>% 
-#   filter(condition == 'fine')
+df_NMI <- read.csv('data_for_R_NMI_hierarchical.csv')
 
-# df_NMI_mixed <- df %>% 
-#   filter(condition == 'mixed')
-
-# df_NMI_coarse <- df %>% 
-#   filter(condition == 'coarse')
-
-agg_NMI <- df %>%
+agg_NMI <- df_NMI %>%
   summarize(mean = mean(value),
             sd = sd(value))
 
-grand_mean <- agg_NMI$mean
-grand_sd <- agg_NMI$sd
+grand_mean_NMI <- agg_NMI$mean
+grand_sd_NMI <- agg_NMI$sd
 
 # calculate a region of practical equivalence with zero according to recommendation by Kruschke (2018)
-rope <- c(-0.1*grand_sd, 0.1*grand_sd)
+rope_NMI <- c(-0.1*grand_sd_NMI, 0.1*grand_sd_NMI)
 
-priors <- list(muM = grand_mean, muSD = grand_sd)
+priors_NMI <- list(muM = grand_mean_NMI, muSD = grand_sd_NMI)
 
-fine_generic_concepts <- df %>% 
+fine_generic_concepts <- df_NMI %>% 
   filter(hierarchy_level == 0, condition == 'fine') %>% 
   select(value, condition)
 
-fine_specific_concepts <- df %>% 
+fine_specific_concepts <- df_NMI %>% 
   filter(hierarchy_level == 4, condition == 'fine') %>% 
   select(value, condition)
 
-mixed_generic_concepts <- df %>% 
+mixed_generic_concepts <- df_NMI %>% 
   filter(hierarchy_level == 0, condition == 'mixed') %>% 
   select(value, condition)
 
-mixed_specific_concepts <- df %>% 
+mixed_specific_concepts <- df_NMI %>% 
   filter(hierarchy_level == 4, condition == 'mixed') %>% 
   select(value, condition)
 
-coarse_generic_concepts <- df %>% 
+coarse_generic_concepts <- df_NMI %>% 
   filter(hierarchy_level == 0, condition == 'coarse') %>% 
   select(value, condition)
 
-coarse_specific_concepts <- df %>% 
+coarse_specific_concepts <- df_NMI %>% 
   filter(hierarchy_level == 4, condition == 'coarse') %>% 
   select(value, condition)
 
 # either load or generate models
-#load("BEST_fine_hierarchical.Rda")
-#load("BEST_mixed_hierarchical.Rda")
-#load("BEST_coarse_hierarchical.Rda")
-BEST_fine_hierarchical <- BESTmcmc(fine_specific_concepts$value, fine_generic_concepts$value, priors=priors, parallel=TRUE)
-BEST_mixed_hierarchical <- BESTmcmc(mixed_specific_concepts$value, mixed_generic_concepts$value, priors=priors, parallel=TRUE)
-BEST_coarse_hierarchical <- BESTmcmc(coarse_specific_concepts$value, coarse_generic_concepts$value, priors=priors, parallel=TRUE)
+#load("BEST_fine_NMI_hierarchical.Rda")
+#load("BEST_mixed_NMI_hierarchical.Rda")
+#load("BEST_coarse_NMI_hierarchical.Rda")
+BEST_fine_NMI_hierarchical <- BESTmcmc(fine_specific_concepts$value, fine_generic_concepts$value, priors=priors_NMI, parallel=TRUE)
+BEST_mixed_NMI_hierarchical <- BESTmcmc(mixed_specific_concepts$value, mixed_generic_concepts$value, priors=priors_NMI, parallel=TRUE)
+BEST_coarse_NMI_hierarchical <- BESTmcmc(coarse_specific_concepts$value, coarse_generic_concepts$value, priors=priors_NMI, parallel=TRUE)
 
 # check for convergence
-print(BEST_fine_hierarchical)
-print(BEST_mixed_hierarchical)
-print(BEST_coarse_hierarchical)
+print(BEST_fine_NMI_hierarchical)
+print(BEST_mixed_NMI_hierarchical)
+print(BEST_coarse_NMI_hierarchical)
 # -> all models converged
 
-Diff_fine <- (BEST_fine_hierarchical$mu1 - BEST_fine_hierarchical$mu2)
+Diff_fine <- (BEST_fine_NMI_hierarchical$mu1 - BEST_fine_NMI_hierarchical$mu2)
 meanDiff_fine <- round(mean(Diff_fine), 2)
-hdiDiff_fine <- round(hdi(BEST_fine_hierarchical$mu1 - BEST_fine_hierarchical$mu2),2)
-plotAll(BEST_fine_hierarchical)
-plot(BEST_fine_hierarchical, ROPE=rope)
-summary(BEST_fine_hierarchical)
+hdiDiff_fine <- round(hdi(BEST_fine_NMI_hierarchical$mu1 - BEST_fine_NMI_hierarchical$mu2),2)
+plotAll(BEST_fine_NMI_hierarchical)
+plot(BEST_fine_NMI_hierarchical, ROPE=rope_NMI)
+summary(BEST_fine_NMI_hierarchical)
 # CrI does not include 0
-# 97.6% probability that the difference in means is larger than 0 (pd)
+# 97.8% probability that the difference in means is larger than 0 (pd)
 # 3% in ROPE
 
-Diff_mixed <- (BEST_mixed_hierarchical$mu1 - BEST_mixed_hierarchical$mu2)
+Diff_mixed <- (BEST_mixed_NMI_hierarchical$mu1 - BEST_mixed_NMI_hierarchical$mu2)
 meanDiff_mixed <- round(mean(Diff_mixed), 2)
-hdiDiff_mixed <- round(hdi(BEST_mixed_hierarchical$mu1 - BEST_mixed_hierarchical$mu2),2)
-plotAll(BEST_mixed_hierarchical)
-plot(BEST_mixed_hierarchical, ROPE=rope)
-summary(BEST_mixed_hierarchical)
+hdiDiff_mixed <- round(hdi(BEST_mixed_NMI_hierarchical$mu1 - BEST_mixed_NMI_hierarchical$mu2),2)
+plotAll(BEST_mixed_NMI_hierarchical)
+plot(BEST_mixed_NMI_hierarchical, ROPE=rope_NMI)
+summary(BEST_mixed_NMI_hierarchical)
 # CrI includes 0
 # 91.5% probability that the difference in means is smaller than 0 (pd), 
 # i.e. negative and large enough
 # 19% in ROPE
 
-Diff_coarse <- (BEST_coarse_hierarchical$mu1 - BEST_coarse_hierarchical$mu2)
+Diff_coarse <- (BEST_coarse_NMI_hierarchical$mu1 - BEST_coarse_NMI_hierarchical$mu2)
 meanDiff_coarse <- round(mean(Diff_coarse), 2)
-hdiDiff_coarse <- round(hdi(BEST_coarse_hierarchical$mu1 - BEST_coarse_hierarchical$mu2), 2)
-plotAll(BEST_coarse_hierarchical)
-plot(BEST_coarse_hierarchical, ROPE=rope)
-summary(BEST_coarse_hierarchical)
+hdiDiff_coarse <- round(hdi(BEST_coarse_NMI_hierarchical$mu1 - BEST_coarse_NMI_hierarchical$mu2), 2)
+plotAll(BEST_coarse_NMI_hierarchical)
+plot(BEST_coarse_NMI_hierarchical, ROPE=rope_NMI)
+summary(BEST_coarse_NMI_hierarchical)
 # CrI does not include 0
 # 100% probability that the difference in means is larger than 0 (pd)
 # 0% in ROPE
 
 # save all models for reproducibility
-write.csv(BEST_fine_hierarchical, "BEST_fine_hierarchical.csv", row.names=FALSE, quote=FALSE) 
-save(BEST_fine_hierarchical,file="BEST_fine_hierarchical.Rda")
-write.csv(BEST_fine_hierarchical, "BEST_mixed_hierarchical.csv", row.names=FALSE, quote=FALSE) 
-save(BEST_fine_hierarchical,file="BEST_mixed_hierarchical.Rda")
-write.csv(BEST_coarse_hierarchical, "BEST_coarse_hierarchical.csv", row.names=FALSE, quote=FALSE) 
-save(BEST_coarse_hierarchical,file="BEST_coarse_hierarchical.Rda")
+write.csv(BEST_fine_NMI_hierarchical, "BEST_fine_NMI_hierarchical.csv", row.names=FALSE, quote=FALSE) 
+save(BEST_fine_NMI_hierarchical,file="BEST_fine_NMI_hierarchical.Rda")
+write.csv(BEST_fine_NMI_hierarchical, "BEST_mixed_NMI_hierarchical.csv", row.names=FALSE, quote=FALSE) 
+save(BEST_fine_NMI_hierarchical,file="BEST_mixed_NMI_hierarchical.Rda")
+write.csv(BEST_coarse_NMI_hierarchical, "BEST_coarse_NMI_hierarchical.csv", row.names=FALSE, quote=FALSE) 
+save(BEST_coarse_NMI_hierarchical,file="BEST_coarse_NMIhierarchical.Rda")
+
+# effectiveness----------------------------
+
+df_effectiveness <- read.csv('data_for_R_effectiveness_hierarchical.csv')
+
+agg_effectiveness <- df_effectiveness %>%
+  summarize(mean = mean(value),
+            sd = sd(value))
+
+grand_mean_effectiveness <- agg_effectiveness$mean
+grand_sd_effectiveness <- agg_effectiveness$sd
+
+# calculate a region of practical equivalence with zero according to recommendation by Kruschke (2018)
+rope_effectiveness <- c(-0.1*grand_sd_effectiveness, 0.1*grand_sd_effectiveness)
+
+priors <- list(muM = grand_mean_effectiveness, muSD = grand_sd_effectiveness)
+
+fine_generic_concepts <- df_effectiveness %>% 
+  filter(hierarchy_level == 0, condition == 'fine') %>% 
+  select(value, condition)
+
+fine_specific_concepts <- df_effectiveness %>% 
+  filter(hierarchy_level == 4, condition == 'fine') %>% 
+  select(value, condition)
+
+mixed_generic_concepts <- df_effectiveness %>% 
+  filter(hierarchy_level == 0, condition == 'mixed') %>% 
+  select(value, condition)
+
+mixed_specific_concepts <- df_effectiveness %>% 
+  filter(hierarchy_level == 4, condition == 'mixed') %>% 
+  select(value, condition)
+
+coarse_generic_concepts <- df_effectiveness %>% 
+  filter(hierarchy_level == 0, condition == 'coarse') %>% 
+  select(value, condition)
+
+coarse_specific_concepts <- df_effectiveness %>% 
+  filter(hierarchy_level == 4, condition == 'coarse') %>% 
+  select(value, condition)
+
+# either load or generate models
+load("BEST_fine_effectiveness_hierarchical.Rda")
+load("BEST_mixed_effectiveness_hierarchical.Rda")
+load("BEST_coarse_effectiveness_hierarchical.Rda")
+BEST_fine_effectiveness_hierarchical <- BESTmcmc(fine_specific_concepts$value, fine_generic_concepts$value, priors=priors, parallel=TRUE)
+BEST_mixed_effectiveness_hierarchical <- BESTmcmc(mixed_specific_concepts$value, mixed_generic_concepts$value, priors=priors, parallel=TRUE)
+BEST_coarse_effectiveness_hierarchical <- BESTmcmc(coarse_specific_concepts$value, coarse_generic_concepts$value, priors=priors, parallel=TRUE)
+
+# check for convergence
+print(BEST_fine_effectiveness_hierarchical)
+print(BEST_mixed_effectiveness_hierarchical)
+print(BEST_coarse_effectiveness_hierarchical)
+# -> all models converged
+
+Diff_fine <- (BEST_fine_effectiveness_hierarchical$mu1 - BEST_fine_effectiveness_hierarchical$mu2)
+meanDiff_fine <- round(mean(Diff_fine), 2)
+hdiDiff_fine <- round(hdi(BEST_fine_effectiveness_hierarchical$mu1 - BEST_fine_effectiveness_hierarchical$mu2),2)
+plotAll(BEST_fine_effectiveness_hierarchical)
+plot(BEST_fine_effectiveness_hierarchical, ROPE=rope_effectiveness)
+summary(BEST_fine_effectiveness_hierarchical)
+# CrI includes 0
+# 94.9% probability that the difference in means is larger than 0 (pd)
+# 21% in ROPE
+
+Diff_mixed <- (BEST_mixed_effectiveness_hierarchical$mu1 - BEST_mixed_effectiveness_hierarchical$mu2)
+meanDiff_mixed <- round(mean(Diff_mixed), 2)
+hdiDiff_mixed <- round(hdi(BEST_mixed_effectiveness_hierarchical$mu1 - BEST_mixed_effectiveness_hierarchical$mu2),2)
+plotAll(BEST_mixed_effectiveness_hierarchical)
+plot(BEST_mixed_effectiveness_hierarchical, ROPE=rope_effectiveness)
+summary(BEST_mixed_effectiveness_hierarchical)
+# CrI does not include 0
+# 99.4% probability that the difference in means is smaller than 0 (pd), 
+# i.e. negative and large enough
+# 3% in ROPE
+
+Diff_coarse <- (BEST_coarse_effectiveness_hierarchical$mu1 - BEST_coarse_effectiveness_hierarchical$mu2)
+meanDiff_coarse <- round(mean(Diff_coarse), 2)
+hdiDiff_coarse <- round(hdi(BEST_coarse_effectiveness_hierarchical$mu1 - BEST_coarse_effectiveness_hierarchical$mu2), 2)
+plotAll(BEST_coarse_effectiveness_hierarchical)
+plot(BEST_coarse_effectiveness_hierarchical, ROPE=rope_effectiveness)
+summary(BEST_coarse_effectiveness_hierarchical)
+# CrI does not include 0
+# 100% probability that the difference in means is smaller than 0 (pd)
+# 0% in ROPE
+
+# save all models for reproducibility
+write.csv(BEST_fine_effectiveness_hierarchical, "BEST_fine_effectiveness_hierarchical.csv", row.names=FALSE, quote=FALSE) 
+save(BEST_fine_effectiveness_hierarchical,file="BEST_fine_effectiveness_hierarchical.Rda")
+write.csv(BEST_fine_effectiveness_hierarchical, "BEST_mixed_effectiveness_hierarchical.csv", row.names=FALSE, quote=FALSE) 
+save(BEST_fine_effectiveness_hierarchical,file="BEST_mixed_effectiveness_hierarchical.Rda")
+write.csv(BEST_coarse_effectiveness_hierarchical, "BEST_coarse_effectiveness_hierarchical.csv", row.names=FALSE, quote=FALSE) 
+save(BEST_coarse_effectiveness_hierarchical,file="BEST_coarse_effectiveness_hierarchical.Rda")
+
+# consistency----------------------------
+
+df_consistency <- read.csv('data_for_R_consistency_hierarchical.csv')
+
+agg_consistency <- df_consistency %>%
+  summarize(mean = mean(value),
+            sd = sd(value))
+
+grand_mean_consistency <- agg_consistency$mean
+grand_sd_consistency <- agg_consistency$sd
+
+# calculate a region of practical equivalence with zero according to recommendation by Kruschke (2018)
+rope_consistency <- c(-0.1*grand_sd_consistency, 0.1*grand_sd_consistency)
+
+priors <- list(muM = grand_mean_consistency, muSD = grand_sd_consistency)
+
+fine_generic_concepts <- df_consistency %>% 
+  filter(hierarchy_level == 0, condition == 'fine') %>% 
+  select(value, condition)
+
+fine_specific_concepts <- df_consistency %>% 
+  filter(hierarchy_level == 4, condition == 'fine') %>% 
+  select(value, condition)
+
+mixed_generic_concepts <- df_consistency %>% 
+  filter(hierarchy_level == 0, condition == 'mixed') %>% 
+  select(value, condition)
+
+mixed_specific_concepts <- df_consistency %>% 
+  filter(hierarchy_level == 4, condition == 'mixed') %>% 
+  select(value, condition)
+
+coarse_generic_concepts <- df_consistency %>% 
+  filter(hierarchy_level == 0, condition == 'coarse') %>% 
+  select(value, condition)
+
+coarse_specific_concepts <- df_consistency %>% 
+  filter(hierarchy_level == 4, condition == 'coarse') %>% 
+  select(value, condition)
+
+# either load or generate models
+load("BEST_fine_consistency_hierarchical.Rda")
+load("BEST_mixed_consistency_hierarchical.Rda")
+load("BEST_coarse_consistency_hierarchical.Rda")
+BEST_fine_consistency_hierarchical <- BESTmcmc(fine_specific_concepts$value, fine_generic_concepts$value, priors=priors, parallel=TRUE)
+BEST_mixed_consistency_hierarchical <- BESTmcmc(mixed_specific_concepts$value, mixed_generic_concepts$value, priors=priors, parallel=TRUE)
+BEST_coarse_consistency_hierarchical <- BESTmcmc(coarse_specific_concepts$value, coarse_generic_concepts$value, priors=priors, parallel=TRUE)
+
+# check for convergence
+print(BEST_fine_consistency_hierarchical)
+print(BEST_mixed_consistency_hierarchical)
+print(BEST_coarse_consistency_hierarchical)
+# -> all models converged
+
+Diff_fine <- (BEST_fine_consistency_hierarchical$mu1 - BEST_fine_consistency_hierarchical$mu2)
+meanDiff_fine <- round(mean(Diff_fine), 2)
+hdiDiff_fine <- round(hdi(BEST_fine_consistency_hierarchical$mu1 - BEST_fine_consistency_hierarchical$mu2),2)
+plotAll(BEST_fine_consistency_hierarchical)
+plot(BEST_fine_consistency_hierarchical, ROPE=rope_consistency)
+summary(BEST_fine_consistency_hierarchical)
+# CrI does not include 0
+# 97.3% probability that the difference in means is larger than 0 (pd)
+# 2% in ROPE
+
+Diff_mixed <- (BEST_mixed_consistency_hierarchical$mu1 - BEST_mixed_consistency_hierarchical$mu2)
+meanDiff_mixed <- round(mean(Diff_mixed), 2)
+hdiDiff_mixed <- round(hdi(BEST_mixed_consistency_hierarchical$mu1 - BEST_mixed_consistency_hierarchical$mu2),2)
+plotAll(BEST_mixed_consistency_hierarchical)
+plot(BEST_mixed_consistency_hierarchical, ROPE=rope_consistency)
+summary(BEST_mixed_consistency_hierarchical)
+# CrI includes 0
+# 86.2% probability that the difference in means is smaller than 0 (pd), 
+# 9% in ROPE
+
+Diff_coarse <- (BEST_coarse_consistency_hierarchical$mu1 - BEST_coarse_consistency_hierarchical$mu2)
+meanDiff_coarse <- round(mean(Diff_coarse), 2)
+hdiDiff_coarse <- round(hdi(BEST_coarse_consistency_hierarchical$mu1 - BEST_coarse_consistency_hierarchical$mu2), 2)
+plotAll(BEST_coarse_consistency_hierarchical)
+plot(BEST_coarse_consistency_hierarchical, ROPE=rope_consistency)
+summary(BEST_coarse_consistency_hierarchical)
+# CrI does not include 0
+# 99.8% probability that the difference in means is smaller than 0 (pd)
+# 0% in ROPE
+
+# save all models for reproducibility
+write.csv(BEST_fine_consistency_hierarchical, "BEST_fine_consistency_hierarchical.csv", row.names=FALSE, quote=FALSE) 
+save(BEST_fine_consistency_hierarchical,file="BEST_fine_consistency_hierarchical.Rda")
+write.csv(BEST_fine_consistency_hierarchical, "BEST_mixed_consistency_hierarchical.csv", row.names=FALSE, quote=FALSE) 
+save(BEST_fine_consistency_hierarchical,file="BEST_mixed_consistency_hierarchical.Rda")
+write.csv(BEST_coarse_consistency_hierarchical, "BEST_coarse_consistency_hierarchical.csv", row.names=FALSE, quote=FALSE) 
+save(BEST_coarse_consistency_hierarchical,file="BEST_coarse_consistency_hierarchical.Rda")
--- BayesianAnalysis/Hierarchical_Analyses_new.R
+++ BayesianAnalysis/Hierarchical_Analyses_new.R
@@ -1,3 +1,9 @@
+## Title: Analysis of entropy scores per concept hierarchy level
+## Description: Analyses reported in section 3.3 of the paper
+## Author: Kristina Kobrock
+## Contact: kristina.kobrock@uni-osnabrueck.de
+## Last Edit: 2026-05-27
+
 setwd(normalizePath(dirname(rstudioapi::getActiveDocumentContext()$path)))
 
 # BEST package is no longer active, but can still be downloaded from CRAN archive
@@ -19,12 +25,6 @@ df_NMI <- read.csv('data_for_R_NMI_hierarchical.csv')
 
 df_NMI <- df_NMI %>%
   dplyr::rename(entropy = value)
-agg_NMI <- df_NMI %>%   
-  summarize(mean = mean(entropy),
-            sd = sd(entropy))
-
-grand_mean_NMI <- agg_NMI$mean
-grand_sd_NMI <- agg_NMI$sd
 
 df_NMI$condition <- factor(df_NMI$condition, levels = c("mixed", "fine", "coarse"))
 #contrasts(df_NMI$condition) <- contr.sum(2)
@@ -43,6 +43,47 @@ pp_check(fit_NMI_hier)
 describe_posterior(fit_NMI_hier, rope_range = rope_range(fit_NMI_hier))
 # no difference between fine and mixed
 # significant difference between coarse and mixed
+
+# difference between NMI scores for mixed vs. fine condition and specific concepts:
+# NMI----------------------------
+
+agg_NMI <- df_NMI %>%   
+  summarize(mean = mean(entropy),
+            sd = sd(entropy))
+
+grand_mean_NMI <- agg_NMI$mean
+grand_sd_NMI <- agg_NMI$sd
+
+# calculate a region of practical equivalence with zero according to recommendation by Kruschke (2018)
+rope_NMI <- c(-0.1*grand_sd_NMI, 0.1*grand_sd_NMI)
+
+priors_NMI <- list(muM = grand_mean_NMI, muSD = grand_sd_NMI)
+
+fine_specific_concepts <- df_NMI %>% 
+  filter(hierarchy_level == 4, condition == 'fine') %>% 
+  dplyr::select(entropy, condition)
+
+mixed_specific_concepts <- df_NMI %>% 
+  filter(hierarchy_level == 4, condition == 'mixed') %>% 
+  dplyr::select(entropy, condition)
+
+# either load or generate models
+#load("BEST_specific_fine_mixed_NMI_hierarchical.Rda")
+BEST_specific_fine_mixed_NMI_hierarchical <- BESTmcmc(fine_specific_concepts$entropy, mixed_specific_concepts$entropy, priors=priors_NMI, parallel=TRUE)
+
+# check for convergence
+print(BEST_specific_fine_mixed_NMI_hierarchical)
+
+Diff_specific <- (BEST_specific_fine_mixed_NMI_hierarchical$mu1 - BEST_specific_fine_mixed_NMI_hierarchical$mu2)
+meanDiff_specific <- round(mean(Diff_specific), 2)
+hdiDiff_specific <- round(hdi(BEST_specific_fine_mixed_NMI_hierarchical$mu1 - BEST_specific_fine_mixed_NMI_hierarchical$mu2),2)
+plotAll(BEST_specific_fine_mixed_NMI_hierarchical)
+plot(BEST_specific_fine_mixed_NMI_hierarchical, ROPE=rope_NMI)
+summary(BEST_specific_fine_mixed_NMI_hierarchical)
+# CrI does not include 0
+# 99% probability that the difference in means is larger than 0 (pd)
+# 1% in ROPE
+
 
 # effectiveness----------------------------
 
